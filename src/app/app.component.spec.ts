a15e9dc1d564ec8febbcd9267bac9f91db209376
ddmodifi <denise@wemodifi.com>
2019-09-12 11:42:21 -0700

    add false test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,4 +29,8 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
+
+  it('should return true', () => {
+    expect(false).toBe(true);
+  });
 });
